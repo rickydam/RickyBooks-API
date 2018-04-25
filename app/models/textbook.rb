00857821c62ce9b70c858b4c320aa6190f8fc17a
Ricky Dam <RickyDamOttawa@gmail.com>
2018-04-25 16:42:24 -0400

    Initialize project to begin restructure

--- app/models/textbook.rb
+++
@@ -1,9 +0,0 @@
-class Textbook < ApplicationRecord
-  validates :textbook_title,      presence: true
-  validates :textbook_author,     presence: true
-  validates :textbook_edition,    presence: true
-  validates :textbook_condition,  presence: true
-  validates :textbook_type,       presence: true
-  validates :textbook_coursecode, presence: true
-  validates :textbook_price,      presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
-# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  root 'welcome#index'
-  resources :textbooks
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
