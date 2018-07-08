6400162671c55a7372a278e0b7314ae9b134b3c0
Ricky Dam <RickyDamOttawa@gmail.com>
2018-07-08 02:25:45 -0400

    Remove unused method

--- app/controllers/textbooks_controller.rb
+++ app/controllers/textbooks_controller.rb
@@ -89,10 +89,4 @@ class TextbooksController < ApiController
       :textbook_price
     )
   end
-
-  def set_s3_direct_post
-    @s3_direct_post = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}",
-                                               success_action_status: '201',
-                                               acl: 'public-read')
-  end
 end
