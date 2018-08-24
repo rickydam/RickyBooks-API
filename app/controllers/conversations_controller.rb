e7d40c97e511708eec19cae697a23392dacd9d45
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-24 17:46:56 -0400

    Edit the conversations show action to also render the last message

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,15 +7,34 @@ class ConversationsController < ApiController
 
   def show
     conversations = Conversation.where(:sender_id => params[:id]) + Conversation.where(:recipient_id => params[:id])
-    render :json => conversations,
-           :include => {
-               :recipient => {
-                   :only => :name
-               },
-               :sender => {
-                   :only => :name
-               }
-           }
+    conversations_json = conversations.as_json(
+        {
+            :include => [
+                recipient: {
+                    only: [:name]
+                },
+                sender: {
+                    only: [:name]
+                }
+            ]
+        }
+    )
+
+    last_messages = []
+    conversations.each do |conversation|
+      last_message_json = conversation.messages.last.as_json(
+          {
+              :include => {
+                  :user => {
+                      :only => :name
+                  }
+              }
+          }
+      )
+      last_messages.push(last_message_json)
+    end
+
+    render :json => {conversations: conversations_json, last_messages: last_messages}
   end
 
   def create
