2f7a6e2a6aab03e2b34e86f6c94690b73e2aa14a
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-25 15:47:50 -0400

    Add create action to the NotifyItemsController

--- app/controllers/notify_items_controller.rb
+++ app/controllers/notify_items_controller.rb
@@ -5,3 +5,25 @@ class NotifyItemsController < ApiController
     notify_items = NotifyItem.all
     render :json => notify_items
   end
+
+  def create
+    notify_item = NotifyItem.new(notify_items_params)
+    if notify_item.save
+    else
+      render json: {
+          status: 'ERROR',
+          data: notify_item.errors
+      }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def notify_items_params
+    params.permit(
+      :user_id,
+      :category,
+      :input
+    )
+  end
+end
