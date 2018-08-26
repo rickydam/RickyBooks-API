dbdcc15d8811692a8c38b213070870449f6afb7a
Ricky Dam <RickyDamOttawa@gmail.com>
2018-08-26 00:23:26 -0400

    Edit notify_results action to get the user from the token not the param

--- app/controllers/notify_items_controller.rb
+++ app/controllers/notify_items_controller.rb
@@ -21,41 +21,43 @@ class NotifyItemsController < ApiController
   end
 
   def notify_results
-    user = User.find(params[:user_id])
-    notify_items = user.notify_items
+    authenticate_with_http_token do |token|
+      user = User.find_by_token(token)
+      notify_items = user.notify_items
 
-    results = []
+      results = []
 
-    notify_items.each do |item|
-      category = item.category
-      input = item.input
+      notify_items.each do |item|
+        category = item.category
+        input = item.input
 
-      case category
-      when 'Title'
-        textbooks = Textbook.where('textbook_title ILIKE ?', "%#{input}%")
-        textbooks.each do |textbook|
-          results.push(textbook)
+        case category
+        when 'Title'
+          textbooks = Textbook.where('textbook_title ILIKE ?', "%#{input}%")
+          textbooks.each do |textbook|
+            results.push(textbook)
+          end
+        when 'Author'
+          textbooks = Textbook.where('textbook_author ILIKE ?', "%#{input}%")
+          textbooks.each do |textbook|
+            results.push(textbook)
+          end
+        when 'Coursecode'
+          textbooks = Textbook.where('textbook_coursecode ILIKE ?', "%#{input}%")
+          textbooks.each do |textbook|
+            results.push(textbook)
+          end
+        else
+          # No action required
         end
-      when 'Author'
-        textbooks = Textbook.where('textbook_author ILIKE ?', "%#{input}%")
-        textbooks.each do |textbook|
-          results.push(textbook)
-        end
-      when 'Coursecode'
-        textbooks = Textbook.where('textbook_coursecode ILIKE ?', "%#{input}%")
-        textbooks.each do |textbook|
-          results.push(textbook)
-        end
-      else
-        # No action required
       end
-    end
 
-    render :json => results.uniq,
-           :include => {
-               :user => {:only => :name},
-               :images => {:only => :url}
-           }
+      render :json => results.uniq,
+             :include => {
+                 :user => {:only => :name},
+                 :images => {:only => :url}
+             }
+    end
   end
 
   private
